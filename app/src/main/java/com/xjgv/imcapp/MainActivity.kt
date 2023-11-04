8e76497f92b0e503afccd607bc4f28120af4e823
Javier Gomez <javier.gov@outlook.es>
2023-11-03 20:01:02 -0600

    creacion de segundo activity

--- app/src/main/java/com/xjgv/imcapp/MainActivity.kt
+++ app/src/main/java/com/xjgv/imcapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.xjgv.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val editTxt = findViewById<AppCompatEditText>(R.id.edtText)
+
+        btnStart.setOnClickListener {
+            val name = editTxt.text.toString()
+
+            if (name.isNullOrEmpty()){
+                Log.i("xjgv","Boton presionado")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/xjgv/imcapp/ResultActivity.kt
@@ -0,0 +1,13 @@
+package com.xjgv.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        val txtvResult = findViewById<TextView>(R.id.txtvResult)
+    }
+}
